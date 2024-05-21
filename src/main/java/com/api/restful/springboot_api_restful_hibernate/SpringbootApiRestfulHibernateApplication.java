49a4db4226d27c6c0e0f12aa579e1d52dd4bf928
roberto <lakero26@gmail.com>
2024-05-20 22:03:25 -0400

    creacion proyecto

---
+++ src/main/java/com/api/restful/springboot_api_restful_hibernate/SpringbootApiRestfulHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.api.restful.springboot_api_restful_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiRestfulHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiRestfulHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/restful/springboot_api_restful_hibernate/SpringbootApiRestfulHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.restful.springboot_api_restful_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiRestfulHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
