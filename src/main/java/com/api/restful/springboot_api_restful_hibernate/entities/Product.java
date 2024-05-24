b11a0f15c8508305d081a91798480b7654f26410
roberto <lakero26@gmail.com>
2024-05-23 20:51:40 -0400

    Actualizacion clase Product

--- src/main/java/com/api/restful/springboot_api_restful_hibernate/entities/Product.java
+++ src/main/java/com/api/restful/springboot_api_restful_hibernate/entities/Product.java
@@ -1,5 +1,11 @@
 package com.api.restful.springboot_api_restful_hibernate.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
 }
