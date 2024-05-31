834459f92eef613cff678df20491f7d07ab8964a
roberto <rd.navaro@gmail.com>
2024-05-30 22:09:18 -0400

    cracion clase ServicesImpl

--- src/main/java/com/api/restful/springboot_api_restful_hibernate/services/ProductServiceImpl.java
+++ src/main/java/com/api/restful/springboot_api_restful_hibernate/services/ProductServiceImpl.java
@@ -1,5 +1,12 @@
 package com.api.restful.springboot_api_restful_hibernate.services;
 
-public class ProductServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.api.restful.springboot_api_restful_hibernate.repositories.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository repository;
 }
