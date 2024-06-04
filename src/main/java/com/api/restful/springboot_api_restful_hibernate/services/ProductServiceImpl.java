661053398da35c9cb50a6ecefbb6b4538ba8ed6c
roberto <rd.navaro@gmail.com>
2024-06-03 22:59:25 -0400

    comentariosProductService

--- src/main/java/com/api/restful/springboot_api_restful_hibernate/services/ProductServiceImpl.java
+++ src/main/java/com/api/restful/springboot_api_restful_hibernate/services/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.api.restful.springboot_api_restful_hibernate.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.api.restful.springboot_api_restful_hibernate.repositories.ProductRepository;
 
@@ -9,4 +10,14 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    //MOSTRAR TODO
+
+    //BUSCAR POR ID
+
+    //AGREGAR
+
+    //ACTUALIZAR
+
+    //ELIMINAR
 }
