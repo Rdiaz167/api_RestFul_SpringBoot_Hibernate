8448f5154f63040e3511b462aabe9c42e7b5f332
roberto <rd.navaro@gmail.com>
2024-06-18 22:40:51 -0400

    actualizar controller

--- src/main/java/com/api/restful/springboot_api_restful_hibernate/controllers/ProductController.java
+++ src/main/java/com/api/restful/springboot_api_restful_hibernate/controllers/ProductController.java
@@ -5,12 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +50,14 @@ public class ProductController {
     }
 
     //ACTUALIZAR
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> productOptional = service.update(id, product);
+        if(productOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
 
     //ELIMINAR
 
