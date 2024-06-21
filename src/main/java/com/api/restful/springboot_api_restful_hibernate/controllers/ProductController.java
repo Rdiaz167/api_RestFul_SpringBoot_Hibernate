a784149cb954c59108639284e78b6dd661c1d2b3
roberto <rd.navaro@gmail.com>
2024-06-20 21:16:19 -0400

    eliminar controller

--- src/main/java/com/api/restful/springboot_api_restful_hibernate/controllers/ProductController.java
+++ src/main/java/com/api/restful/springboot_api_restful_hibernate/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +61,12 @@ public class ProductController {
     }
 
     //ELIMINAR
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Product> productOptional = service.delete(id);
+        if(productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
